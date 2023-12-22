05a8d178eac5f62c8aa6f9884dc6787957915995
Jagadish Sau <jsau5337@gmail.com>
2023-12-22 22:12:02 +0530

    Project Initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './styles/App.css'
+import AppRoute from './routes/AppRoute'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <AppRoute/>
+    </>
+  )
+}
+
+export default App
