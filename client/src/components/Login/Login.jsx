3bc32f5a5089f87db2d12b898989294811eee0c2
Jagadish Sau <jsau5337@gmail.com>
2024-03-17 10:12:10 +0530

    update client -> Login popup

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,31 +1,52 @@
 import React, { useState } from 'react';
 import "./Login.css"
+import api from '~/api';
+import Notification from '../Notification/Notification';
 
 const App = () => {
-  const [username, setUsername] = useState('');
+
+  const [notification, setNotification] = useState({ type: '', message: '' });
+
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handlePasswordChange = (e) => {
+  const handlePassword = (e) => {
     setPassword(e.target.value);
   };
 
+
+  const isSubmitDisabled = (!email || !password);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await api.post('/login', { email, password });
+    } catch (error) {
+      setNotification({ type: 'res-err', message: error });
+      setEmail('');
+      setPassword('');
+    }
+    window.location.reload();
+  }
+
   return (
     <>
+      <Notification type={notification.type} message={notification.message} />
       <div className="login-popup">
         <h3>Log in</h3>
-        <div className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
 
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username">Email</label>
           <input
-            type="text"
-            id="username"
+            type="email"
+            id="email"
             className="login-input"
-            value={username}
-            onChange={handleUsernameChange}
+            value={email}
+            onChange={handleEmail}
           />
 
           <label htmlFor="password">Password</label>
@@ -34,16 +55,21 @@ const App = () => {
             id="password"
             className="login-input"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={handlePassword}
           />
 
-          <button className='login-btn'>Login</button>
-        </div>
+          <button
+            className='login-btn'
+            type="submit"
+            disabled={isSubmitDisabled}
+            style={isSubmitDisabled ?
+              { backgroundColor: '#007bff', color: 'white', cursor: 'default' }
+              : {}
+            }>Login</button>
+        </form>
       </div>
     </>
   );
 };
 
 export default App;
-
-// ReactDOM.render(<App/>,document.getElementById('root'));
